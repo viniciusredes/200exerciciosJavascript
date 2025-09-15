e02ed496a4143e4a94583a3ab3bf501ecfa8b112
Vinícius Oliveira <vinicius.redes.tecnologia@gmail.com>
2025-09-15 09:53:31 -0300

    Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima.

---
+++ exercicio04.js
@@ -0,0 +1,4 @@
+//Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima.
+let nome = "Vinícius";
+let sobrenome = "Oliveira";
+let nomeCompleto = nome + " " + sobrenome;
