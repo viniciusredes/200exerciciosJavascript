ac59290ae5d07412b049817244714dc0e8320189
Vinícius Oliveira <vinicius.redes.tecnologia@gmail.com>
2025-09-15 11:36:29 -0300

    Declare três variáveis e atribua valores numéricos a elas. Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados.

---
+++ exercicio07.js
@@ -0,0 +1,9 @@
+//Declare três variáveis e atribua valores numéricos a elas. Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados.
+
+let num1 = 100;
+let num2 = 100.10;
+let num3 = -15;
+num1 == num2;
+num1 < num2 > num3;
+num1 !== num2 > num3;
+num1 === parseInt(num2);
