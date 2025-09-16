a0cd4cc5b1cb91cea362a1ec81db54b1dc7bb506
Vinícius Oliveira <vinicius.redes.tecnologia@gmail.com>
2025-09-16 16:35:10 -0300

    Usando um laço do-while, imprima todos os números ímpares de 1 a 20.

--- exercicio25.js
+++ exercicio25.js
@@ -1,6 +1,6 @@
 //Usando um laço while, imprima os números de 10 a 1 (em ordem decrescente).
 let num = 10
-do{
-console.log(num)
-num--
-}while(num >= 1)
+while(num >= 1){
+  console.log(num)
+  num--
+}
---
+++ exercicio26.js
@@ -0,0 +1,6 @@
+//Usando um laço do-while, imprima todos os números ímpares de 1 a 20.
+let num = 1
+do{
+console.log(num)
+num++
+}while(num <= 20)
