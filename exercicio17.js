cbb429910e9114ee969736e463849070f378e508
Vinícius Oliveira <vinicius.redes.tecnologia@gmail.com>
2025-09-16 11:33:27 -0300

    Escreva um programa que determina se um número é par ou ímpar.

---
+++ exercicio17.js
@@ -0,0 +1,8 @@
+//Escreva um programa que determina se um número é par ou ímpar.
+let num = 2;
+//let ePar = num%2 == 0 ? "É par" : "É impar";
+if(num%2 == 0){
+  console.log("É par");
+}else{
+  console.log("É impar");
+}
