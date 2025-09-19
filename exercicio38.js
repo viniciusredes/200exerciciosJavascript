918294ff34e027eb8f41d7ed5ddccb5710936d67
Vinícius Oliveira <vinicius.redes.tecnologia@gmail.com>
2025-09-19 17:03:05 -0300

    Usando um laço do-while, imprima a soma dos cubos dos primeiros 10 números naturais.

---
+++ exercicio38.js
@@ -0,0 +1,7 @@
+//Usando um laço do-while, imprima a soma dos cubos dos primeiros 10 números naturais.
+
+let i = 1
+while(i <= 10){
+  console.log(i**3);
+  i++
+}
---
+++ exercicio39.js
@@ -0,0 +1,6 @@
+//Usando um laço for, crie um programa que calcule o fatorial de um número.
+
+let numero = 10
+for(let i = numero; i > 0; i--){
+  numero = numero * i
+}
