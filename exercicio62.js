d97c5e3a43d60afde7f1e133bba913e7a2b0a3ff
Vinícius Oliveira <vinicius.redes.tecnologia@gmail.com>
2025-09-26 13:33:20 -0300

    Escreva uma função que receba um número e retorne uma string repetida aquele número de vezes.

---
+++ exercicio62.js
@@ -0,0 +1,6 @@
+//Escreva uma função que receba um número e retorne uma string repetida aquele número de vezes.
+function repetePalavra(x){
+  for(let i= 1; i<=x;i++){
+    console.log("palavra")  
+  }
+}
