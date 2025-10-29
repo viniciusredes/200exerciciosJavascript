7f2976b1993a6c062a08c8febed3c46d7b295698
Vinícius Oliveira <vinicius.redes.tecnologia@gmail.com>
2025-10-29 17:40:18 -0300

    Exercício 102: Dado um array de strings, retorne uma única string que é a concatenação de todas as strings do array, separadas por vírgulas.

---
+++ exercicio102.js
@@ -0,0 +1,4 @@
+//Exercício 102: Dado um array de strings, retorne uma única string que é a concatenação de todas as strings do array, separadas por vírgulas.
+const myArray = ['A','B',"C","D"]
+const joinArray = myArray.join(",")
+console.log(joinArray)
